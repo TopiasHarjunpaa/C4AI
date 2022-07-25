2fdd4943339242d06d3a15032fd9080ec008e63a
Topias Harjunpää <topias.harjunpaa@gmail.com>
2022-07-25 22:31:17 +0300

    create skeleton for ai service tests

---
+++ src/tests/services/ai_service_test.py
@@ -0,0 +1,13 @@
+import unittest
+from services.ai_service import AiService
+from services.board_service import BoardService
+
+class TestAiService(unittest.TestCase):
+    def setUp(self):
+        self.width = 640
+        self.height = 480
+        self.board = BoardService(self.width, self.height)
+        self.ai = AiService(self.board)
+
+    def test_nothing(self):
+        pass
