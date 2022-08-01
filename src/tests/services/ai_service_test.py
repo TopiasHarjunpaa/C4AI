2fdf2d20e3f7a6007b26f0339e6f266abd87f534
Topias Harjunpää <topias.harjunpaa@gmail.com>
2022-08-01 20:36:13 +0300

    tests for ai service

--- src/tests/services/ai_service_test.py
+++ src/tests/services/ai_service_test.py
@@ -4,7 +4,9 @@ from services.ai_service import AiService
 from services.board_service import BoardService
 from services.situation_service import SituationService
 from tests.test_grids import (G_AE1, G_AE2, G_AE3, G_AE4, G_1W1,
-                              G_1W2, G_WO1, G_SF1, G_SF2)
+                              G_1W2, G_WO1, G_SF1, G_SF2, G_HO1,
+                              G_UD1, G_DD1, G_VE1)
+from tests.test_locs import L1W, L2W, L1T1, L1T2, L1F1, L1F2, L2TW1, L2F1
 
 
 class TestAiService(unittest.TestCase):
@@ -15,6 +17,47 @@ class TestAiService(unittest.TestCase):
         self.situation = SituationService(self.board)
         self.ai = AiService(self.situation)
 
+    def copy_grid_allows_changes_without_modifying_original(self):
+        original_grid1 = self.board.grid
+        original_grid2 = G_AE4
+        copy_grid1 = self.ai._copy_grid(original_grid1)
+        copy_grid2 = self.ai._copy_grid(original_grid2)
+        self.assertEqual(original_grid1, copy_grid1)
+        self.assertEqual(original_grid2, copy_grid2)
+        copy_grid1[0][0] = 9
+        self.assertNotEqual(original_grid1, copy_grid1)
+        self.assertNotEqual(original_grid1[0][0], copy_grid1[0][0])
+        self.assertEqual(copy_grid1[0][0], 9)
+        self.assertEqual(original_grid1[0][0], 0)
+
+    def test_calculate_next_move_basic_returns_correct_choice(self):
+        location = self.ai.calculate_next_move_basic(G_AE1, 1)
+        self.assertEqual(location, (5, 2))
+        location = self.ai.calculate_next_move_basic(G_AE4, 2)
+        self.assertEqual(location, (5, 3))
+        location = self.ai.calculate_next_move_basic(G_SF2, 1)
+        self.assertEqual(location, (1, 4))
+        location = self.ai.calculate_next_move_basic(G_1W1, 2)
+        self.assertEqual(location, (4, 2))
+
+    def test_count_values_correctly(self):
+        value = self.ai._count_values(L1W, 1)
+        self.assertEqual(value, 10000)
+        value = self.ai._count_values(L1W, 2)
+        self.assertEqual(value, 0)
+        value = self.ai._count_values(L1T1, 1)
+        self.assertEqual(value, 10)
+        value = self.ai._count_values(L1T2, 1)
+        self.assertEqual(value, 10)
+        value = self.ai._count_values(L1F1, 1)
+        self.assertEqual(value, 0)
+        value = self.ai._count_values(L1F2, 2)
+        self.assertEqual(value, 0)
+        value = self.ai._count_values(L2TW1, 2)
+        self.assertEqual(value, 2)
+        value = self.ai._count_values(L2F1, 2)
+        self.assertEqual(value, 0)
+
     def test_positional_values_returns_correct_value(self):
         value = self.ai._get_positional_values(G_AE1, 1)
         self.assertEqual(value, 2)
@@ -22,6 +65,24 @@ class TestAiService(unittest.TestCase):
     def test_get_vertical_values(self):
         value = self.ai._get_vertical_values(G_AE1, 1)
         self.assertEqual(value, 0)
+    
+    def test_get_inc_diagonal_values(self):
+        value = self.ai._get_inc_diagonal_values(G_HO1, 1)
+        self.assertEqual(value, 0)
+        value = self.ai._get_inc_diagonal_values(G_HO1, 2)
+        self.assertEqual(value, 6)
+        value = self.ai._get_inc_diagonal_values(G_UD1, 2)
+        self.assertEqual(value, 10012)
+
+    def test_get_dec_diagonal_values(self):
+        value = self.ai._get_dec_diagonal_values(G_WO1, 1)
+        self.assertEqual(value, 0)        
+        value = self.ai._get_dec_diagonal_values(G_WO1, 2)
+        self.assertEqual(value, 0)   
+        value = self.ai._get_dec_diagonal_values(G_DD1, 1)
+        self.assertEqual(value, 10010) 
+        value = self.ai._get_dec_diagonal_values(G_1W2, 2)
+        self.assertEqual(value, 2) 
 
     def test_heuristic_value_returns_correct_value(self):
         value = self.ai._heuristic_value(G_AE1, 1)
@@ -30,6 +91,18 @@ class TestAiService(unittest.TestCase):
         self.assertEqual(value, 1)
         value = self.ai._heuristic_value(G_AE3, 2)
         self.assertEqual(value, 2)
+
+    def test_check_terminal_node(self):
+        answer = self.ai._check_terminal_node(G_AE1, 1, 2)
+        self.assertEqual(answer, None)
+        answer = self.ai._check_terminal_node(G_VE1, 1, 2)
+        self.assertEqual(answer, math.inf)
+        answer = self.ai._check_terminal_node(G_VE1, 2, 1)
+        self.assertEqual(answer, -math.inf)
+        answer = self.ai._check_terminal_node(G_WO1, 1, 2)
+        self.assertEqual(answer, 0)
+        answer = self.ai._check_terminal_node(G_WO1, 2, 1)
+        self.assertEqual(answer, 0)
 
     def test_minimax_finds_terminal_situations(self):
         value = self.ai._minimax(G_1W1, 2, 4, True)[0]
@@ -45,9 +118,8 @@ class TestAiService(unittest.TestCase):
         self.assertEqual(location, None)
         value, location = self.ai._minimax(G_SF1, 1, 7, True)
         self.assertEqual(value, -math.inf)
+
+    def test_minimax_returns_best_move(self):
         value, location = self.ai._minimax(G_SF2, 2, 6, True)
         self.assertEqual(value, math.inf)
         self.assertEqual(location, (1, 4))
-
-    def test_minimax_returns_best_move(self):
-        pass
---
+++ src/tests/test_locs.py
@@ -0,0 +1,19 @@
+#Player 1 win
+L1W = [1,1,1,1]
+
+#Player 2 win
+L2W = [2,2,2,2]
+
+#Player 1 threes
+L1T1 = [1,1,1,0]
+L1T2 = [1,0,1,1]
+
+#Player 1 threes no win
+L1F1 = [1,1,1,2]
+L1F2 = [1,2,1,1]
+
+#Player 2 twos
+L2TW1 = [2,0,2,0]
+
+#Player 2 twos no win
+L2F1 = [0,2,1,2]
